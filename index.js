386fa8a990f75f185b6b719dca1997fbe9ad0f8a
abUwUser <pedro.santos.cartaxo@gmail.com>
2021-03-13 13:24:22 -0300

    useless lines of code

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ import { getModule } from '@vizality/webpack';
 import { patch, unpatch } from "@vizality/patcher"
 
 export default class ScreenshareFree extends Plugin {
-    constructor(stuff){
-        super(stuff)
-        this.originalType;
-    }
     start () {
         patch("screenshare-free", getModule("getCurrentUser"), "getCurrentUser", (args, res) => {
             res.premiumType = 2
